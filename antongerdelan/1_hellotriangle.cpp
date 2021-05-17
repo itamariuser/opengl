66e1695dedd7f75cf769471fda3e4717375be982
itama <zooby>
2021-05-17 20:15:01 +0300

    Initial commit

---
+++ antongerdelan/1_hellotriangle.cpp
@@ -0,0 +1,119 @@
+//#include <glad/glad.h>
+//#define GLFW_INCLUDE_NONE
+//#include <GLFW/glfw3.h>
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//
+//#include "cstdlib"
+//#include "cstdio"
+//
+//int main(void)
+//{
+//    GLFWwindow* window;
+//
+//    if (!glfwInit())
+//        exit(EXIT_FAILURE);
+//
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+//
+//    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    glfwMakeContextCurrent(window);
+//    gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
+//    glfwSwapInterval(1);
+//
+//    // ------------------------- START HERE ----------------------------------
+//
+//    float points[] = {
+//            0.0f,  0.5f,  0.0f,
+//            0.5f, -0.5f,  0.0f,
+//            -0.5f, -0.5f,  0.0f,
+//            0.f,  0.f,  0.0f,
+//            1.f, -1.f,  0.0f,
+//            -1.f, -1.f,  0.0f
+//    };
+//
+//    GLuint vbo = 0;
+//    glGenBuffers(1, &vbo);
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//    glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), points, GL_STATIC_DRAW);
+//
+//    GLuint vao = 0;
+//    glGenVertexArrays(1, &vao);
+//    glBindVertexArray(vao);
+//    glEnableVertexAttribArray(0);
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+//
+//    const char* vertex_shader =
+//            "#version 400\n"
+//            "in vec3 vp;"
+//            "void main() {"
+//            "  gl_Position = vec4(vp, 1.0);"
+//            "}";
+//
+//    const char* fragment_shader =
+//            "#version 400\n"
+//            "out vec4 frag_colour;"
+//            "void main() {"
+//            "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+//            "}";
+//
+//    const char* fragment_shader_cyan =
+//            "#version 400\n"
+//            "out vec4 frag_colour;"
+//            "void main() {"
+//            "  frag_colour = vec4(0.0, 1.0, 1.0, 1.0);"
+//            "}";
+//
+//    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+//    glShaderSource(vs, 1, &vertex_shader, NULL);
+//    glCompileShader(vs);
+//    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fs, 1, &fragment_shader, NULL);
+//    glCompileShader(fs);
+//
+//    GLuint fs_cyan = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fs_cyan, 1, &fragment_shader_cyan, NULL);
+//    glCompileShader(fs_cyan);
+//
+//    GLuint shader_programme = glCreateProgram();
+//    glAttachShader(shader_programme, fs);
+//    glAttachShader(shader_programme, vs);
+//    glLinkProgram(shader_programme);
+//
+//    GLuint shader_program_cyan = glCreateProgram();
+//    glAttachShader(shader_program_cyan, fs_cyan);
+//    glAttachShader(shader_program_cyan, vs);
+//    glLinkProgram(shader_program_cyan);
+//
+//
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        // wipe the drawing surface clear
+//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//        glUseProgram(shader_programme);
+//        glBindVertexArray(vao);
+//        // draw points 0-3 from the currently bound VAO with current in-use shader
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+//
+//        glUseProgram(shader_program_cyan);
+//        glDrawArrays(GL_TRIANGLES, 3, 3);
+//        // update other events like input handling
+//        glfwPollEvents();
+//        // put the stuff we've been drawing onto the display
+//        glfwSwapBuffers(window);
+//    }
+//
+//    glfwDestroyWindow(window);
+//
+//    glfwTerminate();
+//    exit(EXIT_SUCCESS);
+//}
